3ccb9569fe27dd789c7b7b8d0f767c99561f8657
Ondřej Perný <pernyond@fit.cvut.cz>
2018-04-25 21:42:58 +0200

    basic interface

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
 # Python semestral work
+
+def rotateRight():
+    pass
+
+def rotateLeft():
+    pass
+
+def mirror():
+    pass
+
+def invert():
+    pass
+
+def greyscale():
+    pass
+
+def lighten():
+    pass
+
+def darken():
+    pass
+
+def highlightEdge():
+    pass
+
+#interface
+print('Program pro upravu obrazku, zadej jmeno souboru, ktery chces upravit: ')
+file = input()
+print('''Seznam možností (vyber číslo a stiskni enter)'
+      1. exit - vypne program
+      2. nový soubor - nechá uživatele zadat nový soubor
+      3. rotace do prava
+      4. rotace do leva
+      5. zrcadlení obrazu
+      6. inverze obrazu
+      7. převod do odstínu šedi
+      8. zesvětlení
+      9. ztmavení
+      10.zvýraznění hran
+      ''')
