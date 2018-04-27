ac9fd93ca537351f76a180557eed889ee734911e
Ondřej Perný <pernyond@fit.cvut.cz>
2018-04-27 15:24:39 +0200

    mirror and invert

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Python semestral work
 
+import sys
+sys.excepthook = sys.__excepthook__
 import os
+import numpy
+from PIL import Image
 # clear display
 print('\033[2J', end='')
 
@@ -44,12 +48,18 @@ def loadInput():
         print('Zadaný soubor nelze načíst, zadejte jiný:')
         file = input('Zadejte jméno souboru:\n')
     print('\033[2J', end='')
-    return file
+    fileData = numpy.array(Image.open(file))
+    return file, fileData
+
+def saveOutput(fileData, file, change):
+    f = Image.fromarray(fileData, 'RGB')
+    f.save(file + "_" + change + ".png")
 
 flag = 0
 # interface
 print('Program pro upravu obrazku, zadej jmeno souboru, ktery chces upravit: ')
-file = loadInput()
+# file, fileData = loadInput()
+file, fileData = 'test.png', numpy.array(Image.open('test.png'))
 
 while True:
     if flag == -1:
@@ -74,11 +84,15 @@ while True:
         print('\033[2J', end='')
         exit()
     elif flag == '1':
-        file = loadInput()
+        file, fileData = loadInput()
     # elif flag == '2':
     # elif flag == '3':
-    # elif flag == '4':
-    # elif flag == '5':
+    elif flag == '4':
+        fileData = numpy.flip(fileData, 1)
+        saveOutput(fileData, file, "mirror")
+    elif flag == '5':
+        fileData[::] = 255 - fileData[::]
+        saveOutput(fileData, file, "invert")
     # elif flag == '6':
     # elif flag == '7':
     # elif flag == '8':
